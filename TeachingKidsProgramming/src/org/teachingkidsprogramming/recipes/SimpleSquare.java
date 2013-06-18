0c64a723e1f65c461cd138e28661edb3679ecfaf
WaRP TuX <javascriptnut@gmail.com>
2013-06-18 14:07:05 -0700

    2nd committing with for>>int loop

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,5 +1,6 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 public class SimpleSquare
@@ -9,9 +10,12 @@ public class SimpleSquare
     Tortoise.show();
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
-    //      Change the color of the line the tortoise draws to "blue" --#4
-    Tortoise.move(50);
-    Tortoise.turn(90);
+    for (int i = 0; i < 4; i++)
+    {
+      Tortoise.setPenColor(Colors.Blues.Blue);
+      Tortoise.move(50);
+      Tortoise.turn(90);
+    }
     //  Repeat --#5.2
   }
 }
