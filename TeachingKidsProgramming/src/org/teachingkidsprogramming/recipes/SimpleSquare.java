d89dc66443bf94657b0bd40bbd4f1920d24f3f36
WaRP TuX <javascriptnut@gmail.com>
2013-06-18 15:22:35 -0700

    Variation random Tanner-Amittai

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -2,26 +2,26 @@ package org.teachingkidsprogramming.recipes;
 
 import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.windows.MessageBox;
 
 public class SimpleSquare
 {
-  static int penwidth = 20;
-  static int angle    = (-360 / -4);
+  //static int penwidth = 20;
+  //static int angle = (-360 / -4);
   public static void main(String[] args) throws Exception
   {
     Tortoise.hide();
     Tortoise.setSpeed(10);
-    int sides = 600;
+    int sides = MessageBox.askForNumericalInput("How many sides? A negative number will not work.");
     for (int i = 0; i < sides; i++)
     {
       Tortoise.setPenColor(Colors.getRandomColor());
-      Tortoise.setPenWidth(penwidth);
-      penwidth++;
+      Tortoise.setPenWidth(i * i * i);
+      //   penwidth++;
       //   angle += 10;
-      Tortoise.move(1);
-      Tortoise.turn(1);
+      Tortoise.move(i * 3);
+      Tortoise.turn(360.0 / sides);
       // Tortoise.setPenWidth(penwidth);
-      //  Math.random();
     }
   }
 }
