1ebcb2043ad04f9c965483fe8122fd845fd50aeb
Tanner Saffert <Tsaffert@hotmail.com>
2013-06-23 21:30:26 -0700

    Fist attempt at the thing

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
@@ -1,20 +1,34 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class DigiFlower
 {
   public static void main(String[] args)
   {
-    //    Show the tortoise --#1
-    //    Make the tortoise move as fast as possible --#7
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    Tortoise.setPenWidth(3);
+    drawOctogon();
     //    Make the background silver --#8
-    //    Make the line the tortoise draws 3 pixels wide --#20
     //    createColorPalette (recipe below) --#9
     //    Do the following 15 times --#19
     //      drawOctogon (recipe below) --#10
     //      Turn the tortoise 1/15th of 360 degrees to the right --#18
     //    Repeat
-    //    ------------- Recipe for createColorPalette --#9
-    //    Color 1 is red --#3
+  }
+  public static void drawOctogon()
+  {
+    for (int i = 1; i <= 8; i++)
+    {
+      //      Change the color of the line the tortoise draws to the next color on the color wheel --#4
+      Tortoise.move(50);
+      Tortoise.turn(360 / 8);
+    }
+  }
+  public static void createColorPalette()
+  {
+    //  Color 1 is red --#3
     //    Color 2 is dark orange --#11
     //    Color 3 is gold --#12
     //    Color 4 is yellow --#13
@@ -26,11 +40,5 @@ public class DigiFlower
     //    Add color 3 to the color wheel --#15
     //    Add color 2 to the color wheel --#16
     //    Add color 1 to the color wheel --#17
-    //    ------------- Recipe for drawOctogon --#10
-    //    Do the following 8 times --#6
-    //      Change the color of the line the tortoise draws to the next color on the color wheel --#4
-    //      Move the tortoise 50 pixels --#2
-    //      Turn the tortoise 1/8th of 360 degrees to the right --#5
-    //    Repeat  
   }
 }
