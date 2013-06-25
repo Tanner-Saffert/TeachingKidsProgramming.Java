74250f111e330e47c016a3e202d1632aa2145746
Tanner Saffert <Tsaffert@hotmail.com>
2013-06-24 23:15:22 -0700

    done

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
@@ -1,44 +1,44 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.awt.Color;
+
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 public class DigiFlower
 {
   public static void main(String[] args)
   {
+    //Beginning values
     Tortoise.show();
-    Tortoise.setSpeed(10);
+    Tortoise.setSpeed(5);
+    Tortoise.getBackgroundWindow().setColor(Colors.Grays.Silver);
     Tortoise.setPenWidth(3);
-    drawOctogon();
-    //    Make the background silver --#8
-    //    createColorPalette (recipe below) --#9
-    //    Do the following 15 times --#19
-    //      drawOctogon (recipe below) --#10
-    //      Turn the tortoise 1/15th of 360 degrees to the right --#18
-    //    Repeat
-  }
-  public static void drawOctogon()
-  {
-    for (int i = 1; i <= 8; i++)
+    //Color Variables
+    Color c1 = Colors.Reds.Red;
+    Color c2 = Colors.Oranges.DarkOrange;
+    Color c3 = Colors.Yellows.Gold;
+    Color c4 = Colors.Yellows.Yellow;
+    //Adding to the colorwheel
+    ColorWheel.addColor(c1);
+    ColorWheel.addColor(c2);
+    ColorWheel.addColor(c3);
+    ColorWheel.addColor(c4);
+    ColorWheel.addColor(c4);
+    ColorWheel.addColor(c3);
+    ColorWheel.addColor(c2);
+    ColorWheel.addColor(c1);
+    //shape drawing function
+    for (int i = 0; i < 15; i++)
     {
-      //      Change the color of the line the tortoise draws to the next color on the color wheel --#4
-      Tortoise.move(50);
-      Tortoise.turn(360 / 8);
+      for (int j = 0; j < 8; j++)
+      {
+        Tortoise.setPenColor(ColorWheel.getNextColor());
+        Tortoise.move(50);
+        Tortoise.turn(45);
+      }
+      Tortoise.turn(24);
     }
-  }
-  public static void createColorPalette()
-  {
-    //  Color 1 is red --#3
-    //    Color 2 is dark orange --#11
-    //    Color 3 is gold --#12
-    //    Color 4 is yellow --#13
-    //    Add color 1 to the color wheel --#3.1
-    //    Add color 2 to the color wheel --#11.1
-    //    Add color 3 to the color wheel --#12.1
-    //    Add color 4 to the color wheel --#13.1
-    //    Add color 4 to the color wheel --#14
-    //    Add color 3 to the color wheel --#15
-    //    Add color 2 to the color wheel --#16
-    //    Add color 1 to the color wheel --#17
   }
 }
